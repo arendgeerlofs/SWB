770de8d1fccc4460c02ae40b58da18f0e620e45f
arendgeerlofs <arendgeerlofs@hotmail.nl>
2024-07-15 15:53:59 +0200

    Update run.py

--- run.py
+++ run.py
@@ -1,58 +1,68 @@
 """
 run file
 """
-# from run_functions import all_scenarios, stoch_plot_param, run_exec, run_var_plot, run_two_var_heatmap
-from sa import param_space_behaviour#, GSA
-# from visualisation import visualise, plot_avg, plot_for_one, SWB_gif
+from run_functions import all_scenarios, stoch_plot_param, run_exec, run_two_var_heatmap, run_over_model, run_resilience, run_degree_SWB
+from sa import param_space_behaviour, GSA
+from visualisation import visualise, plot_avg, plot_agent, SWB_gif
 from parameters import params
-# from scenarios import scenarios
-# from stochs import param_dict
+from scenarios import scenarios
+from stochs import param_dict
+import numpy as np
 
-ITERATIONS = 200
+ITERATIONS = 250
 RUNS = 1
-SAMPLES = 1
+SAMPLES = 3
+edit_params = ["segregation", "beta", "rec_intervention_factor", "intervention_gap", "hist_len", "fb_fin", "dummy"]
+bounds = [(0, 10), (0, 20), (0.5, 2), (1, 50), (1, 50), (0, 2), (0, 5)]
 
-# One run of the model
-# output = run_exec(params, ITERATIONS)
 
-# Plot simple plots
-# plot_for_one(output)
-# plot_avg(output)
+# run_degree_SWB(params, RUNS, ITERATIONS)
 
-# # Stochastic plot for 1 parameter with bounds and param samples
+# # Run the model once
+output = run_exec(params, ITERATIONS)
+
+# # Plot simple plots
+# plot_agent(output, title_add="_BA")
+# plot_avg(output, title_add="_BA")
+
+
+# # Gathering data of the system
+# if __name__ == '__main__':
+#     param_space_behaviour(params, ITERATIONS, 4096 , edit_params, bounds)
+
+# # One-Factor-At-A-Time 
+# # 
+# run_over_model(params, RUNS, ITERATIONS, networks=["SDA"], visualisations=["plot_agent", "plot_avg"], title_add="test")
+
+# Stochastic plot for 1 parameter with bounds and param samples
 # for param in param_dict:
 #     print(f"------{param}------")
 #     params_copy = params.copy()
-#     stoch_plot_param(params_copy, RUNS, ITERATIONS, param, param_dict[param], SAMPLES, plot_components=True, title_add="no_rec/")
+#     stoch_plot_param(params_copy, RUNS, ITERATIONS, param, param_dict[param], SAMPLES, plot_components=True, title_add="OFAT/")
 
-
-# # Var plot
-# run_var_plot(params, RUNS, ITERATIONS)
-
-# Run all scenarios and plot average SWB
+# # Run all scenarios and plot average SWB
 # all_scenarios(params, scenarios, ITERATIONS)
 
-# Plot histogram of SWB over time
+# # SWB gif of network
+# visualise(model, output)
+
+# # Plot histogram of SWB over time
 # SWB_gif(model, output, ITERATIONS, fps=5, name="SWB_hist", xlabel="SWB score",
 #         ylabel="Amount", xlim=(0, 10), ylim=[0, 10])
 
-# SWB gif of network
-# visualise(model, output)
+# # Resilience tests
+# run_resilience(params, RUNS, ITERATIONS, "soc_cap_base", [0.01, 1], SAMPLES, [60, 120, 180, 240], [5/6, 4/6, 3/6, 2/6], "fin", title_add="fin")
+# run_resilience(params, RUNS, ITERATIONS, "soc_cap_inf", [0.25, 4], SAMPLES, [60, 120, 180, 240], [5/6, 4/6, 3/6, 2/6], "fin", title_add="fin")
 
-edit_params = ["segregation", "beta", "fin_event_prob", "nonfin_event_prob", "event_size", "net_upd_freq", "rec_intervention_factor", "intervention_gap", "hist_len", "fb_fin", "fb_nonfin", "soc_cap_base", "soc_cap_inf", "dummy"]
-bounds = [(0, 2), (0, 20), (0, 1), (0, 1), (0, 5), (0, 1), (0.5, 2), (1, 50), (1, 50), (0, 2), (0, 2), (0.01, 1), (0.25, 4), (0, 10)]
+# run_resilience(params, RUNS, ITERATIONS, "soc_cap_base", [0.01, 1], SAMPLES, [60, 120, 180, 240], [5/6, 4/6, 3/6, 2/6], "nonfin", title_add="nonfin")
+# run_resilience(params, RUNS, ITERATIONS, "soc_cap_inf", [0.25, 4], SAMPLES, [60, 120, 180, 240], [5/6, 4/6, 3/6, 2/6], "nonfin", title_add="nonfin")
 
+# # Heatmap
+# run_two_var_heatmap(params, RUNS, ITERATIONS, (3, 3), ["intervention_gap", "hist_len"], [(1, 7), (1, 7)], title_add="hist_int_testtest", hist_gap_comb=False)
+
+# # Specific run functions
 # # Sensitivity Analysis
 # if __name__ == '__main__':
 #     sa = GSA(params, ITERATIONS, 1024, edit_params, bounds, sa_type="Pawn")
 #     print("---------------")
 #     print(sa)
-
-# System data
-if __name__ == '__main__':
-    data = param_space_behaviour(params, ITERATIONS, 2, edit_params, bounds)
-    # print("---------------")
-    # print(data)
-
-# Heatmap
-# run_two_var_heatmap(params, RUNS, ITERATIONS, (7, 11), ["hist_comb", "rec_intervention_factor"], [(1, 7), (1, 2)], title_add="exp", hist_gap_comb=True)
